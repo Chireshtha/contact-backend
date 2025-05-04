a38a65baf6eda5383ae6057f57057f1eb6f3a7ce
Chireshtha V <160488486+Chireshtha@users.noreply.github.com>
2025-05-04 14:40:35 +0530

    Initial commit for backend

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import Message from './models/Message.js';
+
+dotenv.config();
+
+const app = express();
+app.use (cors());
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("MongoDB Connected"))
+.catch((err)=>console.log("MongoDb Connection Error", err));
+
+app.post('/contact', async(req, res)=>{
+    const {name, email, subject, message} = req.body;
+    try{
+        const newMsg = new Message({name, email, subject, message});
+        await newMsg.save();
+        res.status(200).json({success:true, message:'Message Saved'});
+    }
+    catch(error){
+        console.log("Error Saving Message:", error);
+        res.status(500).json({success:false, message:'Failed to save message'});
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
