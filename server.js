9fc9c6732a2b49ca24dab9b845d0702bd7ad4094
Chireshtha V <160488486+Chireshtha@users.noreply.github.com>
2025-05-11 22:29:17 +0530

    first commit

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const allowedOrigins = [
 
 const corsOptions = {
     origin: function (origin, callback) {
+        console.log("Origin:", origin); 
         if(!origin || allowedOrigins.includes(origin)){
             callback(null, true);
         }
