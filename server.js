1b0e0245296ca9c14c8e158347537743b5bedbc1
Chireshtha V <160488486+Chireshtha@users.noreply.github.com>
2025-05-13 15:46:10 +0530

    first commit

--- server.js
+++ server.js
@@ -35,7 +35,10 @@ app.use(express.json());
 app.get('/', (req, res) => res.send('👋 Contact API is live'));
 
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
     .then(() => console.log("MongoDB Connected"))
     .catch((err) => console.log("MongoDB Connection Error", err));
 
