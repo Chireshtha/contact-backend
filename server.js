9ef6f91ecfdc6aa59c30dff5c27f6eca35b0049a
Chireshtha V <160488486+Chireshtha@users.noreply.github.com>
2025-05-15 13:36:51 +0530

    first commit

--- server.js
+++ server.js
@@ -10,9 +10,10 @@ dotenv.config();
 
 const app = express();
 
+// CORS settings
 const allowedOrigins = [
   'https://chireshtha-brighture-innovation.netlify.app',
-  'https://www.chireshtha-brighture-innovation.netlify.app', // just in case
+  'https://www.chireshtha-brighture-innovation.netlify.app',
   'https://chireshtha-portfolio.netlify.app'
 ];
 
@@ -28,6 +29,7 @@ app.use(cors({
   credentials: true
 }));
 
+// Preflight handling
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
@@ -36,6 +38,14 @@ app.use((req, res, next) => {
     return res.sendStatus(200);
   }
   next();
+});
+
+// Specific OPTIONS route for /subscribe
+app.options('/subscribe', (req, res) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  return res.sendStatus(200);
 });
 
 app.use(express.json());
