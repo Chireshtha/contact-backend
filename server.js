12fc8cd06007a45acd8603b2f530da8a90ecfeb5
Chireshtha V <160488486+Chireshtha@users.noreply.github.com>
2025-05-17 22:05:00 +0530

    first commit

--- server.js
+++ server.js
@@ -16,15 +16,9 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET', 'POST'],
-    credentials: true
+  origin: '*',
+  methods: ['GET', 'POST'],
+  credentials: true
 }));
 app.use(express.json());
 
